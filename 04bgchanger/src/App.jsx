fc776ef3f7b86682dcd1dd348386703a218665fa
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-06-15 00:09:51 +0530

    done

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -3,11 +3,35 @@ import { useState } from 'react'
 
 
 function App() {
-  const [bgColor, setBgColor] = useState("red")
+  const [bgColor, setBgColor] = useState("green")
   return (
-    
-    <div className = "w-full h-screen " style={{backgroundColor: bgColor}}  >
+
+    <div className="w-full h-screen " style={{ backgroundColor: bgColor }}>
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+
+        <div className="flex flex-wrap justify-center  gap-3 shadow-2xs bg-amber-100 rounded-lg px-3 py-2  font-bold tracking-wider ">
+          
+        <button
+          onClick={() => setBgColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setBgColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => setBgColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+
       </div>
+
+    </div>
   )
 }
 
