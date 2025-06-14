eae7f081ec4a0e1bd5edd249619303631d25ed0a
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-06-14 18:51:16 +0530

    project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [color, setColor] = useState("green")
+
+  return (
+   
+      
+      <div className='w-full h-screen' 
+      style={{backgroundColor: color}}>
+
+      </div>
+      
+   
+  )
+}
+
+export default App
