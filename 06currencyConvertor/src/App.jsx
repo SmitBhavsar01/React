c8572df3a6243bccafbdce8e3c2470162301515c
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-06-16 18:31:49 +0530

    currency_convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <>
+      <h1 className='text-4xl bg-orange-500 font-semibold font-serif text-white py-2.5 tracking-wide '>Currency Convertor</h1>
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default App
