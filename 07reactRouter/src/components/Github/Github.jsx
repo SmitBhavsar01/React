185eb518160bef58346e988576572494720b7aee
SmitBhavsar01 <smitbhavsar1992@gmail.com>
2025-06-20 11:10:25 +0530

    loader_added

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 
 function Github() {
     
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/smitbhavsar01')
-        .then(response => response.json() )
-        .then(data => {
-            console.log(data);
-            setData(data)
+      const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/smitbhavsar01')
+    //     .then(response => response.json() )
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
             
-        })
-    },[])
+    //     })
+    // },[])
   return (
-    <div className='text-center m-4 bg-gray-800 text-blue-50 p-4 text-3xl '>Github Followers :  {data.followers}
+    <div className='text-center m-4 bg-gray-800 text-blue-50 p-4 text-3xl '>Github Public Repo :  {data.public_repos}
     
     <img src={data.avatar_url} alt="Smit Bhavsar" width={500} height={300} className='mt-4'/>
     
@@ -24,3 +27,8 @@ function Github() {
 }
 
 export default Github
+
+export  const  githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/smitbhavsar01')
+  return response.json()
+}
